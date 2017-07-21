e3e4c5b4e0c0c604c352cd39111215dc546cce5a
utkarshnath <nath.utkarsh1@gmail.com>
2017-07-21 13:01:05 +0530

    added sessions api

--- app/controllers/public/sessions.js
+++ app/controllers/public/sessions.js
@@ -6,19 +6,19 @@ const { Controller, computed } = Ember;
 export default Controller.extend({
   sessionsByTracks: computed('model.sessions.[]', 'selectedTrackId', function() {
     if (this.get('selectedTrackId')) {
-      return chain(this.get('model.sessions'))
+      return chain(this.get('model.sessions').toArray())
         .filter(['track.id', this.get('selectedTrackId')])
         .orderBy(['track.name', 'startAt'])
         .value();
     } else {
-      return chain(this.get('model.sessions'))
+      return chain(this.get('model.sessions').toArray())
         .orderBy(['track.name', 'startAt'])
         .groupBy('track.name')
         .value();
     }
   }),
   tracks: computed('model.sessions.[]', function() {
-    return chain(this.get('model.sessions'))
+    return chain(this.get('model.sessions').toArray())
       .map('track')
       .uniqBy('id')
       .orderBy('name')
